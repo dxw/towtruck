ecb65d8d823778a4a9f482c75cb94e15fc77f276
rich <rich.james@dxw.com>
2024-09-05 09:38:32 +0100

    Add request to get repos
    Using Octokit and the server added in previous comimits we call the
    Github API and request all repos* that belong to dxw.
    We return them to the client as an array which isn't pretty but
    demonstrates that the app is working.
    
    *The API has a default page limit of 30.

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 import { createServer } from "http";
+import { Octokit } from "@octokit/rest";
+
+const ACCESS_TOKEN = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
+
+const octokit = new Octokit({
+  auth: ACCESS_TOKEN,
+});
+
+const httpServer = createServer(async (_, response) => {
+  const repos = await getRepos({ org: "dxw" });
+
+  return response.end(JSON.stringify(repos));
+});
+
+const getRepos = async ({ org }) => {
+  return octokit
+    .request(`GET /orgs/${org}/repos`, {
+      org,
+    })
+    .then(({ data }) => {
+      return data.map((repo) => ({
+        name: repo.name,
+      }));
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
 
 const PORT = 3000;
 httpServer.listen(PORT, () => {
