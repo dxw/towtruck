701d9d531c4ccb565bcd938cfca2cebffce06a75
rich <rich.james@dxw.com>
2024-09-13 15:32:53 +0100

    Add sorting by updatedAt
    Much the same as the other sort columns we can now sort by updatedAt

--- utils/sorting.js
+++ utils/sorting.js
@@ -21,6 +21,25 @@ export const sortByNumericValue = (repos, sortDirection, key) => {
 };
 
 /**
+ * Sorts repositories by the date they were last updated
+ * @param {UiRepo[]} repos
+ * @param {SortDirection} sortDirection
+ * @returns {UiRepo[]}
+ */
+export const sortByUpdatedAt = (repos, sortDirection) => {
+  if (!sortDirection) return repos;
+
+  if (sortDirection === "asc") {
+    return repos.sort((a, b) => {
+      return a.updatedAtISO8601.localeCompare(b.updatedAtISO8601);
+    });
+  }
+  return repos.sort((a, b) =>
+    b.updatedAtISO8601.localeCompare(a.updatedAtISO8601)
+  );
+};
+
+/**
  * Sorts repositories based on the specified type and direction.
  * @param {"openPrsCount"|"openIssues"} sortBy - The type of sorting to apply, either by open PRs count or open issues.
  * @param {SortDirection} sortDirection - The direction to sort, either "asc" for ascending or "desc" for descending.
@@ -34,6 +53,9 @@ export const sortByType = (repos, sortDirection, sortBy) => {
 
     case "openIssues":
       return sortByNumericValue(repos, sortDirection, "openIssues");
+
+    case "updatedAt":
+      return sortByUpdatedAt(repos, sortDirection);
     default:
       return repos;
   }
