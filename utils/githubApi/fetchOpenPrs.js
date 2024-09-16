bd1ecb91491f3d085bd6391e31efe984ac7439c4
rich <rich.james@dxw.com>
2024-09-16 13:15:01 +0100

    Add open bot PR count to table
    User need to know how many of the open PRs are automated depdency
    updates.
    This column shows how many open PRs exist for a repo where the user
    login is either `renovate[bot]` or `dependabot[bot]`. This may need to
    be finetuned later.

--- utils/githubApi/fetchOpenPrs.js
+++ utils/githubApi/fetchOpenPrs.js
@@ -8,11 +8,20 @@ export const getOpenPRsForRepo = async ({ octokit, repository }) => {
   return octokit.request(repository.pulls_url).then(handlePrsApiResponse);
 };
 
+const depdencyUpdateBots = ["renovate[bot]", "dependabot[bot]"];
+
 /**
  * Returns the length of the PRs array or 0 if there are no PRs
  * @param {any} {data}
  * @returns {number}
  */
 export const handlePrsApiResponse = ({ data }) => {
-  return { openPrCount: data?.length || 0 };
+  const openBotPrCount = data.reduce((acc, pr) => {
+    if (depdencyUpdateBots.includes(pr?.user?.login)) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  return { openPrCount: data?.length || 0, openBotPrCount };
 };
