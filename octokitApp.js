f449617601392e64ff685ef7adc25338dd8f130a
Dan Livings <dan.livings@dxw.com>
2024-10-08 13:08:32 +0100

    Use Base64-encoded private key instead of path to .pem file
    This simplifies the deployment as this allows the private key to be
    passed in as an environment variable, rather than relying on a separate
    file to exist somewhere.

--- octokitApp.js
+++ octokitApp.js
@@ -1,13 +1,12 @@
-import { readFileSync } from "fs";
 import { App, createNodeMiddleware } from "@octokit/app";
 
 const APP_ID = process.env.APP_ID;
-const PRIVATE_KEY_PATH = process.env.PRIVATE_KEY_PATH;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-const privateKey = readFileSync(PRIVATE_KEY_PATH).toString();
+const privateKey = Buffer.from(PRIVATE_KEY, 'base64').toString('ascii');
 
 const app = new App({
   appId: APP_ID,
