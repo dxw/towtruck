6f5730df175553b768f475d662fe29f7034911f7
Dan Livings <dan.livings@dxw.com>
2024-10-15 15:22:41 +0100

    Configure GitHub OAuth setup
    This defines the `REDIRECT_URL_BASE` environment variable which can be
    used to configure which callback URL Towtruck provides when using the
    OAuth authentication flow.
    
    Octokit already provides as part of its middleware the ability to
    authenticate as a GitHub user, so Towtruck just needs to be able to hook
    into this process.

--- octokitApp.js
+++ octokitApp.js
@@ -5,8 +5,10 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+const REDIRECT_URL_BASE = process.env.REDIRECT_URL_BASE;
 
 const privateKey = Buffer.from(PRIVATE_KEY, 'base64').toString('ascii');
+const redirectUrl = `${REDIRECT_URL_BASE}/api/github/oauth/callback`;
 
 const app = new App({
   appId: APP_ID,
@@ -14,6 +16,8 @@ const app = new App({
   oauth: {
     clientId: CLIENT_ID,
     clientSecret: CLIENT_SECRET,
+    redirectUrl,
+    clientType: "github-app"
   },
   webhooks: {
     secret: WEBHOOK_SECRET,
