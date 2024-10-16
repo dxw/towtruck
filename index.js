b7f83d18c25c83a42ec04def7e468bcccd7dde1a
Dan Livings <dan.livings@dxw.com>
2024-10-16 16:29:07 +0100

    Refactor server logic to introduce routes
    This allows basic path parameter recognition in the form of
    `/path/{with}/{parameters}` as well as verbatim paths.
    
    This will allow for things such as redirecting to a dedicated login
    route, or presenting dashboards for specific organisations.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import { createServer } from "http";
 import nunjucks from "nunjucks";
-import { mapRepoFromStorageToUi } from "./utils/index.js";
-import { getQueryParams } from "./utils/queryParams.js";
-import { sortByType } from "./utils/sorting.js";
-import { TowtruckDatabase } from "./db/index.js";
 import { handleWebhooks } from "./webhooks/index.js";
 import { handleOAuthCallback, getTokenOrPromptForLogin } from "./auth/index.js";
+import { handleRoutes } from "./routes/index.js";
 
 nunjucks.configure({
   autoescape: true,
@@ -16,33 +13,11 @@ const httpServer = createServer(async (request, response) => {
   if (await handleOAuthCallback(request, response)) return;
   if (await handleWebhooks(request, response)) return;
 
-  const url = new URL(request.url, `http://${request.headers.host}`);
-
-  if (url.pathname !== "/") {
-    response.writeHead(404);
-    return response.end();
-  }
-
   const token = await getTokenOrPromptForLogin(request, response);
 
   if (!token) return;
 
-  const db = new TowtruckDatabase();
-  const persistedRepoData = db.getAllRepositories();
-  const persistedLifetimeData = db.getAllDependencies();
-
-  const reposForUi = mapRepoFromStorageToUi(persistedRepoData, persistedLifetimeData);
-
-  const { sortDirection, sortBy } = getQueryParams(url);
-
-  const template = nunjucks.render("index.njk", {
-    sortBy,
-    sortDirection,
-    ...reposForUi,
-    repos: sortByType(reposForUi.repos, sortDirection, sortBy),
-  });
-
-  return response.end(template);
+  return await handleRoutes(token, request, response);
 });
 
 const PORT = process.env.PORT || 3000;
