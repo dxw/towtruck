acddb34caa423b433a06f2ba18abd749f2418d06
Dan Livings <dan.livings@dxw.com>
2024-10-17 11:54:31 +0100

    Implement index page
    The root path `/` now displays a list of organisations using Towtruck
    that the user is a member of. Items in the list link to
    organisation-specific dashboards available at `/{org}`. Users that
    aren't members of an organisation will be redirected back to the root
    path, preventing the unauthorised access to information about private
    repositories.

--- routes/handlers.js
+++ routes/handlers.js
@@ -3,24 +3,55 @@ import { mapRepoFromStorageToUi } from "../utils/index.js";
 import { getQueryParams } from "../utils/queryParams.js";
 import { sortByType } from "../utils/sorting.js";
 import { TowtruckDatabase } from "../db/index.js";
+import { OctokitApp } from "../octokitApp.js";
 
-export const index = async (_, request, response) => {
+const getOrgsForUser = async (token) => {
+  const octokit = await OctokitApp.app.oauth.getUserOctokit({ token });
+
+  const { data } = await octokit.request("GET /user/memberships/orgs");
+
+  return data
+    .filter(membership => membership.state === "active")
+    .map(membership => membership.organization.login);
+}
+
+export const index = async (token, request, response) => {
+  const orgs = await getOrgsForUser(token);
+  
+  const template = nunjucks.render("index.njk", {
+    orgs,
+  });
+
+  return response.end(template);
+};
+
+export const org = async (token, request, response, {org}) => {
   const url = new URL(request.url, `http://${request.headers.host}`);
 
+  const orgs = await getOrgsForUser(token);
+
+  if (!orgs.includes(org)) {
+    response.writeHead(302, {
+      "Location": "/",
+    });
+    return response.end();
+  }
+
   const db = new TowtruckDatabase();
-  const persistedRepoData = db.getAllRepositories();
+  const persistedRepoData = db.getAllRepositoriesForOrg(org);
   const persistedLifetimeData = db.getAllDependencies();
 
   const reposForUi = mapRepoFromStorageToUi(persistedRepoData, persistedLifetimeData);
 
   const { sortDirection, sortBy } = getQueryParams(url);
 
-  const template = nunjucks.render("index.njk", {
+  const template = nunjucks.render("dashboard.njk", {
     sortBy,
     sortDirection,
+    org,
     ...reposForUi,
     repos: sortByType(reposForUi.repos, sortDirection, sortBy),
   });
 
   return response.end(template);
-};
+}
