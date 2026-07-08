2400e2e046a31005c521b0797a0e8c3158abd9d8
Michael Huldt <michael.huldt@dxw.com>
2026-07-08 11:17:04 +0100

    Fix tests broken by rebase: add closed PRs mock URL and mostRecentBotPrClosedAt to expected PR data

--- utils/githubApi/fetchAllRepos.test.js
+++ utils/githubApi/fetchAllRepos.test.js
@@ -129,6 +129,9 @@ describe("fetchForRepo", () => {
           },
         ],
       },
+      "https://some.api/pulls?state=closed&sort=updated&direction=desc&per_page=100": {
+        data: [],
+      },
     };
 
     t.mock.method(octokit, "request", async (url) =>
@@ -156,6 +159,7 @@ describe("fetchForRepo", () => {
       openBotPrCount: 1,
       oldestOpenPrOpenedAt: new Date("2024-01-01T12:34:56.789Z"),
       mostRecentPrOpenedAt: new Date("2024-10-10T11:22:33.444Z"),
+      mostRecentBotPrClosedAt: null,
     };
 
     const actual = await fetchForRepo(repository, octokit);
